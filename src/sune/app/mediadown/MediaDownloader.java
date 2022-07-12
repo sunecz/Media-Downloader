c7b2131358ffc0b08584cd9e3cf3d4a4d7395ddf
Sune <sune@suneweb.net>
2022-07-12 13:59:06 +0200

    Increased the application version and updated the application date.

--- src/sune/app/mediadown/MediaDownloader.java
+++ src/sune/app/mediadown/MediaDownloader.java
@@ -120,8 +120,8 @@ public final class MediaDownloader {
 	private static final boolean GENERATE_LISTS = false;
 	
 	public static final String  TITLE   = "Media Downloader";
-	public static final Version VERSION = Version.fromString("00.02.07-dev.6");
-	public static final String  DATE    = "2022-07-11";
+	public static final Version VERSION = Version.fromString("00.02.07-dev.7");
+	public static final String  DATE    = "2022-07-12";
 	public static final String  AUTHOR  = "Sune";
 	public static final Image   ICON    = icon("app.png");
 	
