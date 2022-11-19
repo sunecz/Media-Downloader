3dc1cb39342cec6f08264b3d36990f55680d83c7
Sune <sune@suneweb.net>
2022-11-19 02:49:09 +0100

    Refactored the Log class and added methods for logging to separate files. Also, the default location of the log file has been changed to resources/log/application.log.

--- src/sune/app/mediadown/logging/Log.java
+++ src/sune/app/mediadown/logging/Log.java
@@ -1,15 +1,13 @@
 package sune.app.mediadown.logging;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.lang.StackWalker.StackFrame;
+import java.nio.file.Path;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
-import java.util.Optional;
 import java.util.logging.FileHandler;
 import java.util.logging.Formatter;
 import java.util.logging.Handler;
@@ -19,108 +17,53 @@ import java.util.logging.Logger;
 import java.util.logging.StreamHandler;
 
 import sune.app.mediadown.util.NIO;
-import sune.app.mediadown.util.PathSystem;
 import sune.app.mediadown.util.Utils;
 
 /** @since 00.02.00 */
 public final class Log {
 	
-	private static final class SourceFinder {
-		
-		private static final StackWalker WALKER
-			= StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
-		
-		// Forbid anyone to create an instance of this class
-		private SourceFinder() {
-		}
-		
-		private static final boolean filter(StackFrame frame) {
-			Class<?> c = frame.getDeclaringClass();
-			return c != Log.class && c != SourceFinder.class;
-		}
-		
-		public static final Optional<StackFrame> get() {
-			return WALKER.walk((s) -> s.filter(SourceFinder::filter).findFirst());
-		}
-	}
-	
-	private static final class LogRecordFormatter extends Formatter {
-		
-		private static final ZoneId ZONE_ID = ZoneId.from(ZoneOffset.UTC);
-		private static final String FORMAT  = "%1$s (%3$s) %2$s\n%4$s: %5$s%6$s\n";
-		
-		private static final int LEVEL_DEBUG   = Level.FINE.intValue();
-		private static final int LEVEL_INFO    = Level.INFO.intValue();
-		private static final int LEVEL_WARNING = Level.WARNING.intValue();
-		private static final int LEVEL_ERROR   = Level.SEVERE.intValue();
-		
-		private static final Map<Integer, String> LEVELS_NAMES = Utils.toMap(
-			LEVEL_DEBUG,   "DEBUG",
-			LEVEL_INFO,    "INFO",
-			LEVEL_WARNING, "WARNING",
-			LEVEL_ERROR,   "ERROR"
-		);
-		
-		// Forbid anyone to create an instance of this class
-		private LogRecordFormatter() {
-		}
-		
-		private static final String throwableToString(Throwable throwable) {
-			if((throwable == null)) return "";
-			StringWriter sw = new StringWriter();
-			PrintWriter pw = new PrintWriter(sw);
-			pw.println();
-			throwable.printStackTrace(pw);
-			pw.close();
-			return sw.toString();
-		}
-		
-		private static final String getLevelName(Level level) {
-			return LEVELS_NAMES.getOrDefault(level.intValue(), "UNKNOWN");
-		}
-		
-		@Override
-		public String format(LogRecord record) {
-			ZonedDateTime zdt = ZonedDateTime.ofInstant(record.getInstant(), ZONE_ID);
-			String dateTime = DateTimeFormatter.ISO_DATE_TIME.format(zdt);
-			String source;
-			if((record.getSourceClassName() != null)) {
-				source = record.getSourceClassName();
-				if((record.getSourceMethodName() != null)) {
-					source += "::" + record.getSourceMethodName();
-				}
-			} else {
-				source = record.getLoggerName();
-			}
-			String message = formatMessage(record);
-			String throwable = throwableToString(record.getThrown());
-			String loggerName = record.getLoggerName();
-			String level = getLevelName(record.getLevel());
-			return String.format(FORMAT, dateTime, source, loggerName, level, message, throwable);
-		}
-	}
-	
 	private static final String LOGGER_NAME = "Media-Downloader";
-	private static final String LOG_PATH = "resources/log.txt";
+	/** @since 00.02.08 */
+	private static final Path LOG_DIRECTORY = NIO.localPath("resources/log");
+	private static final Path LOG_PATH = LOG_DIRECTORY.resolve("application.log");
 	private static Logger logger;
 	
 	// Forbid anyone to create an instance of this class
 	private Log() {
 	}
 	
-	public static final void initialize(Level minLevel) {
-		if((minLevel.intValue() == Level.OFF.intValue()))
-			// Do not bother creating any logger
+	private static final void log(Level level, Throwable throwable, String message, Object... args) {
+		if(logger == null) {
 			return;
+		}
+		
+		String sourceClass = null;
+		String sourceMethod = null;
+		
+		StackFrame stackFrame = SourceFinder.stackFrame();
+		if(stackFrame != null) {
+			sourceClass = stackFrame.getClassName();
+			sourceMethod = stackFrame.getMethodName();
+		}
+		
+		logger.logp(level, sourceClass, sourceMethod, String.format(message, args), throwable);
+	}
+	
+	public static final void initialize(Level minLevel) {
+		if(minLevel.intValue() == Level.OFF.intValue()) {
+			return; // Do not bother creating any logger
+		}
+		
 		logger = Logger.getLogger(LOGGER_NAME);
 		logger.setLevel(minLevel);
 		logger.setUseParentHandlers(false);
+		
 		Handler handler;
 		Formatter formatter = new LogRecordFormatter();
 		Exception exception = null;
 		try {
-			NIO.createDir(PathSystem.getPath(Utils.dirname(LOG_PATH)));
-			handler = new FileHandler(LOG_PATH);
+			NIO.createDir(LOG_PATH.getParent());
+			handler = new FileHandler(LOG_PATH.toAbsolutePath().toString());
 			handler.setFormatter(formatter);
 		} catch(SecurityException | IOException ex) {
 			handler = new StreamHandler(System.out, formatter) {
@@ -133,24 +76,14 @@ public final class Log {
 			};
 			exception = ex;
 		}
+		
 		logger.addHandler(handler);
-		if((exception != null)) {
+		
+		if(exception != null) {
 			error(exception, "Failed to initialize logging to a file. Defaulting to the console.");
 		}
+		
 		info("Logger initialized.");
-	}
-	
-	private static final void log(Level level, Throwable throwable, String message, Object... args) {
-		if((logger != null)) {
-			String sourceClass = null, sourceMethod = null;
-			Optional<StackFrame> opt = SourceFinder.get();
-			if((opt.isPresent())) {
-				StackFrame frame = opt.get();
-				sourceClass = frame.getClassName();
-				sourceMethod = frame.getMethodName();
-			}
-			logger.logp(level, sourceClass, sourceMethod, String.format(message, args), throwable);
-		}
 	}
 	
 	public static final void debug(String message, Object... args) {
@@ -183,5 +116,91 @@ public final class Log {
 	
 	public static final void error(Throwable throwable, String message, Object... args) {
 		log(Level.SEVERE, throwable, message, args);
+	}
+	
+	/** @since 00.02.08 */
+	public static final void toPath(Path path, String message, Object... args) throws IOException {
+		NIO.save(path, args.length == 0 ? message : String.format(message, args));
+	}
+	
+	/** @since 00.02.08 */
+	public static final void toUniquePath(String prefix, String message, Object... args)
+			throws IOException {
+		toUniquePath(LOG_DIRECTORY, prefix, message, args);
+	}
+	
+	/** @since 00.02.08 */
+	public static final void toUniquePath(Path directory, String prefix, String message, Object... args)
+			throws IOException {
+		toPath(NIO.tempFile(directory, prefix, ".log"), message, args);
+	}
+	
+	private static final class LogRecordFormatter extends Formatter {
+		
+		private static final ZoneId ZONE_ID = ZoneId.from(ZoneOffset.UTC);
+		private static final String FORMAT  = "%1$s (%3$s) %2$s\n%4$s: %5$s%6$s\n";
+		
+		private static final int LEVEL_DEBUG   = Level.FINE.intValue();
+		private static final int LEVEL_INFO    = Level.INFO.intValue();
+		private static final int LEVEL_WARNING = Level.WARNING.intValue();
+		private static final int LEVEL_ERROR   = Level.SEVERE.intValue();
+		
+		private static final Map<Integer, String> LEVELS_NAMES = Utils.toMap(
+			LEVEL_DEBUG,   "DEBUG",
+			LEVEL_INFO,    "INFO",
+			LEVEL_WARNING, "WARNING",
+			LEVEL_ERROR,   "ERROR"
+		);
+		
+		// Forbid anyone to create an instance of this class
+		private LogRecordFormatter() {
+		}
+		
+		private static final String getLevelName(Level level) {
+			return LEVELS_NAMES.getOrDefault(level.intValue(), "UNKNOWN");
+		}
+		
+		@Override
+		public String format(LogRecord record) {
+			ZonedDateTime zdt = ZonedDateTime.ofInstant(record.getInstant(), ZONE_ID);
+			String dateTime = DateTimeFormatter.ISO_DATE_TIME.format(zdt);
+			
+			String source;
+			if(record.getSourceClassName() != null) {
+				source = record.getSourceClassName();
+				
+				if(record.getSourceMethodName() != null) {
+					source += "::" + record.getSourceMethodName();
+				}
+			} else {
+				source = record.getLoggerName();
+			}
+			
+			String message = formatMessage(record);
+			String throwable = Utils.throwableToString(record.getThrown());
+			String loggerName = record.getLoggerName();
+			String level = getLevelName(record.getLevel());
+			return String.format(FORMAT, dateTime, source, loggerName, level, message, throwable);
+		}
+	}
+	
+	private static final class SourceFinder {
+		
+		private static final StackWalker WALKER
+			= StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
+		
+		// Forbid anyone to create an instance of this class
+		private SourceFinder() {
+		}
+		
+		private static final boolean filter(StackFrame frame) {
+			Class<?> c = frame.getDeclaringClass();
+			return c != Log.class && c != SourceFinder.class;
+		}
+		
+		/** @since 00.02.08 */
+		public static final StackFrame stackFrame() {
+			return WALKER.walk((s) -> s.filter(SourceFinder::filter).findFirst()).orElse(null);
+		}
 	}
 }
