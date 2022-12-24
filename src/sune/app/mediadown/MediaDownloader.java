5c7dd1ab1a42da423a6f490508ebb42a3027242b
Sune <sune@suneweb.net>
2022-12-24 18:38:15 +0100

    Updated the application version to 00.02.08-dev.2.

--- src/sune/app/mediadown/MediaDownloader.java
+++ src/sune/app/mediadown/MediaDownloader.java
@@ -127,8 +127,8 @@ import sune.util.ssdf2.SSDObject;
 public final class MediaDownloader {
 	
 	public static final String  TITLE   = "Media Downloader";
-	public static final Version VERSION = Version.of("00.02.08-dev.1");
-	public static final String  DATE    = "2022-11-25";
+	public static final Version VERSION = Version.of("00.02.08-dev.2");
+	public static final String  DATE    = "2022-12-24";
 	public static final String  AUTHOR  = "Sune";
 	public static final Image   ICON    = icon("app.png");
 	
