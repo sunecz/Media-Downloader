194c39e99b842b110d6eb3834d93a4f375f67b0d
Sune <sune@suneweb.net>
2023-02-11 10:39:27 +0100

    DraggableWindow: Updated the calculation of minimum height. Clean up.

--- src/sune/app/mediadown/gui/DraggableWindow.java
+++ src/sune/app/mediadown/gui/DraggableWindow.java
@@ -2,6 +2,9 @@ package sune.app.mediadown.gui;
 
 import java.util.Objects;
 
+import javafx.collections.ListChangeListener;
+import javafx.collections.ListChangeListener.Change;
+import javafx.collections.ObservableList;
 import javafx.event.EventTarget;
 import javafx.geometry.Bounds;
 import javafx.geometry.Dimension2D;
@@ -10,6 +13,7 @@ import javafx.geometry.Point2D;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseButton;
@@ -27,8 +31,11 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.stage.WindowEvent;
 import sune.app.mediadown.util.FXUtils;
+import sune.app.mediadown.util.Utils;
 
 public class DraggableWindow<T extends Pane> extends Window<StackPane> {
+	
+	protected static final Insets OUTER_INSETS = new Insets(20.0);
 	
 	protected VBox wrapper;
 	protected HBox header;
@@ -116,27 +123,17 @@ public class DraggableWindow<T extends Pane> extends Window<StackPane> {
 		FXUtils.centerWindowOnShow(this, parent, 0.0, -4.0);
 	}
 	
-	public void fitSizeToContent() {
-		FXUtils.once(scene::addPostLayoutPulseListener,
-			scene::removePostLayoutPulseListener,
-			() -> {
-				double height = wrapper.getHeight();
-				Insets padding = pane.getPadding();
-				height += padding.getTop();
-				height += padding.getBottom();
-				setHeight(height);
-			});
-	}
-	
 	public void centerWindowAfterLayoutUpdate() {
-		FXUtils.once(scene::addPostLayoutPulseListener,
+		FXUtils.once(
+			scene::addPostLayoutPulseListener,
 			scene::removePostLayoutPulseListener,
 			() -> {
 				Stage parent;
 				if((parent = (Stage) args.get("parent")) != null) {
 					centerWindow(parent);
 				}
-			});
+			}
+		);
 	}
 	
 	public VBox getWrapper() {
@@ -154,10 +151,9 @@ public class DraggableWindow<T extends Pane> extends Window<StackPane> {
 	private static final class Resizer {
 		
 		private static final double INNER_INSETS = 20.0;
-		private static final double HEADER_MIN_HEIGHT = 30.0;
-		private static final Insets RESIZE_INSETS = new Insets(INNER_INSETS + 5.0);
+		private static final Insets DEFAULT_RESIZE_INSETS = new Insets(INNER_INSETS + 5.0);
 		
-		private final Stage stage;
+		private final DraggableWindow<? extends Pane> window;
 		private final Insets border;
 		
 		private double mx;
@@ -167,34 +163,114 @@ public class DraggableWindow<T extends Pane> extends Window<StackPane> {
 		private Direction dragDirection;
 		private boolean isCursorChanged;
 		
-		private Resizer(Stage stage, Insets border) {
-			this.stage = Objects.requireNonNull(stage);
+		private boolean isSetWidth = false;
+		private boolean isSetHeight = false;
+		
+		private Resizer(DraggableWindow<? extends Pane> window, Insets border) {
+			this.window = Objects.requireNonNull(window);
 			this.border = Objects.requireNonNull(border);
-			stage.resizableProperty().addListener((o, ov, nv) -> updateResizable(nv));
-			updateResizable(stage.isResizable());
-			FXUtils.onWindowShowOnce(stage, () -> {
-				Dimension2D min = minSize((Region) stage.getScene().getRoot());
-				if(stage.getMinWidth()  <= 0.0) stage.setMinWidth (min.getWidth()  + 2.0 * INNER_INSETS);
-				if(stage.getMinHeight() <= 0.0) stage.setMinHeight(min.getHeight() + 3.0 * INNER_INSETS + HEADER_MIN_HEIGHT);
+			
+			window.resizableProperty().addListener((o, ov, nv) -> updateResizable(nv));
+			updateResizable(window.isResizable());
+			
+			FXUtils.onWindowShow(window, () -> {
+				addUpdateListener(window.content);
+				updateMinSize();
 			});
 		}
 		
-		private static final Dimension2D minSize(Region node) {
+		private final void addUpdateListener(Node node) {
+			if(!(node instanceof Parent)) {
+				return;
+			}
+			
+			Parent parent = (Parent) node;
+			parent.getProperties().computeIfAbsent("updateListener", (k) -> {
+				ListChangeListener<? super Node> listener = ((Change<? extends Node> change) -> {
+					while(change.next()) {
+						change.getAddedSubList().forEach(Resizer.this::addUpdateListener);
+						change.getRemoved().forEach(Resizer.this::removeUpdateListener);
+					}
+					
+					updateMinSize();
+				});
+				
+				parent.getChildrenUnmodifiable().addListener(listener);
+				return listener;
+			});
+		}
+		
+		private final void removeUpdateListener(Node node) {
+			if(!(node instanceof Parent)) {
+				return;
+			}
+			
+			Parent parent = (Parent) node;
+			ListChangeListener<? super Node> listener = Utils.cast(parent.getProperties().remove("updateListener"));
+			
+			if(listener != null) {
+				ObservableList<Node> children = parent.getChildrenUnmodifiable();
+				children.forEach(this::removeUpdateListener);
+				children.removeListener(listener);
+			}
+		}
+		
+		private final void updateMinSize() {
+			if(!window.isShowing()) {
+				return;
+			}
+			
+			Dimension2D min = minContentSize(window.content);
+			Insets insets = window.content.getInsets();
+			double headerHeight = window.header.getHeight();
+			
+			if(isSetWidth || window.getMinWidth() <= 0.0) {
+				double outer = OUTER_INSETS.getLeft() + OUTER_INSETS.getRight();
+				double inner = insets.getLeft() + insets.getRight();
+				window.setMinWidth(min.getWidth() + inner + outer);
+				isSetWidth = true;
+			}
+			
+			if(isSetHeight || window.getMinHeight() <= 0.0) {
+				double outer = OUTER_INSETS.getTop() + OUTER_INSETS.getBottom();
+				double inner = insets.getTop() + insets.getBottom();
+				window.setMinHeight(min.getHeight() + inner + outer + headerHeight);
+				isSetHeight = true;
+			}
+		}
+		
+		private static final Dimension2D minContentSize(Region node) {
 			double minWidth = 0.0, minHeight = 0.0;
-			for(Node child : node.getChildrenUnmodifiable()) {
-				if(child instanceof Region) {
+			
+			if(node instanceof Region) {
+				Region r = (Region) node;
+				double minW = r.minWidth (Region.USE_COMPUTED_SIZE);
+				double minH = r.minHeight(Region.USE_COMPUTED_SIZE);
+				minWidth  = Math.max(minWidth,  minW);
+				minHeight = Math.max(minHeight, minH);
+			}
+			
+			if(minWidth <= 0.0 || minHeight <= 0.0) {
+				for(Node child : node.getChildrenUnmodifiable()) {
+					if(!(child instanceof Region)) {
+						continue;
+					}
+					
 					Region r = (Region) child;
-					double minW = r.minWidth(-1.0);
-					double minH = r.minHeight(-1.0);
+					double minW = r.minWidth (Region.USE_COMPUTED_SIZE);
+					double minH = r.minHeight(Region.USE_COMPUTED_SIZE);
+					
 					if(minW <= 0.0 || minH <= 0.0) {
-						Dimension2D min = minSize(r);
+						Dimension2D min = minContentSize(r);
 						minW = Math.max(minW, min.getWidth());
 						minH = Math.max(minH, min.getHeight());
 					}
+					
 					minWidth  = Math.max(minWidth,  minW);
 					minHeight = Math.max(minHeight, minH);
 				}
 			}
+			
 			return new Dimension2D(minWidth, minHeight);
 		}
 		
@@ -202,12 +278,12 @@ public class DraggableWindow<T extends Pane> extends Window<StackPane> {
 	        return Math.round(value * scale) / scale;
 	    }
 		
-		public static final Resizer create(Stage stage) {
-			return create(stage, RESIZE_INSETS);
+		public static final Resizer create(DraggableWindow<? extends Pane> window) {
+			return create(window, DEFAULT_RESIZE_INSETS);
 		}
 		
-		public static final Resizer create(Stage stage, Insets border) {
-			return new Resizer(stage, border);
+		public static final Resizer create(DraggableWindow<? extends Pane> window, Insets resizeInsets) {
+			return new Resizer(window, resizeInsets);
 		}
 		
 		private final void updateResizable(boolean newValue) {
@@ -215,21 +291,21 @@ public class DraggableWindow<T extends Pane> extends Window<StackPane> {
 		}
 		
 		private final void addListeners() {
-			stage.addEventFilter(MouseEvent.MOUSE_PRESSED, this::mousePressed);
-			stage.addEventFilter(MouseEvent.MOUSE_RELEASED, this::mouseReleased);
-			stage.addEventFilter(MouseEvent.MOUSE_MOVED, this::mouseMoved);
-			stage.addEventFilter(MouseEvent.MOUSE_DRAGGED, this::mouseDragged);
+			window.addEventFilter(MouseEvent.MOUSE_PRESSED, this::mousePressed);
+			window.addEventFilter(MouseEvent.MOUSE_RELEASED, this::mouseReleased);
+			window.addEventFilter(MouseEvent.MOUSE_MOVED, this::mouseMoved);
+			window.addEventFilter(MouseEvent.MOUSE_DRAGGED, this::mouseDragged);
 		}
 		
 		private final void removeListeners() {
-			stage.removeEventFilter(MouseEvent.MOUSE_PRESSED, this::mousePressed);
-			stage.removeEventFilter(MouseEvent.MOUSE_RELEASED, this::mouseReleased);
-			stage.removeEventFilter(MouseEvent.MOUSE_MOVED, this::mouseMoved);
-			stage.removeEventFilter(MouseEvent.MOUSE_DRAGGED, this::mouseDragged);
+			window.removeEventFilter(MouseEvent.MOUSE_PRESSED, this::mousePressed);
+			window.removeEventFilter(MouseEvent.MOUSE_RELEASED, this::mouseReleased);
+			window.removeEventFilter(MouseEvent.MOUSE_MOVED, this::mouseMoved);
+			window.removeEventFilter(MouseEvent.MOUSE_DRAGGED, this::mouseDragged);
 		}
 		
 		private final Bounds stageBounds() {
-			return stage.getScene().getRoot().getBoundsInParent();
+			return window.getScene().getRoot().getBoundsInParent();
 		}
 		
 		private final Point2D mousePoint(MouseEvent event) {
@@ -245,7 +321,7 @@ public class DraggableWindow<T extends Pane> extends Window<StackPane> {
 				bounds.getMinX()   + border.getLeft(),
 				bounds.getMinY()   + border.getTop(),
 				bounds.getWidth()  - border.getLeft() - border.getRight(),
-				bounds.getHeight() - border.getTop() - border.getBottom()
+				bounds.getHeight() - border.getTop()  - border.getBottom()
 			);
 		}
 		
@@ -255,6 +331,7 @@ public class DraggableWindow<T extends Pane> extends Window<StackPane> {
 		
 		private final Direction mouseDragDirection(Bounds bounds, Point2D mouse) {
 			Rectangle2D rect = innerRect(bounds);
+			
 			if(mouse.getY() < rect.getMinY()) {
 				if(mouse.getX() <  rect.getMinX()) return Direction.NORTH_WEST; else
 				if(mouse.getX() >= rect.getMaxX()) return Direction.NORTH_EAST;
@@ -278,25 +355,29 @@ public class DraggableWindow<T extends Pane> extends Window<StackPane> {
 		}
 		
 		private final double[] transform(double[] matrix, double[] args, double[] delta) {
-			for(int i = 0; i < 4; ++i)
+			for(int i = 0; i < 4; ++i) {
 				args[i] += matrix[i] * delta[i];
+			}
+			
 			return args;
 		}
 		
 		private final void loadStageArgs() {
-			sx = stage.getX();
-			sy = stage.getY();
-			sw = stage.getWidth();
-			sh = stage.getHeight();
+			sx = window.getX();
+			sy = window.getY();
+			sw = window.getWidth();
+			sh = window.getHeight();
 		}
 		
 		private final void saveStageArgs(double[] args) {
-			double w = Math.max(stage.getMinWidth(),  Math.min(args[2], stage.getMaxWidth()));
-			double h = Math.max(stage.getMinHeight(), Math.min(args[3], stage.getMaxHeight()));
-			if(w == args[2]) stage.setX(args[0]);
-			if(h == args[3]) stage.setY(args[1]);
-			stage.setWidth(w);
-			stage.setHeight(h);
+			double w = Math.max(window.getMinWidth(),  Math.min(args[2], window.getMaxWidth()));
+			double h = Math.max(window.getMinHeight(), Math.min(args[3], window.getMaxHeight()));
+			
+			if(w == args[2]) window.setX(args[0]);
+			if(h == args[3]) window.setY(args[1]);
+			
+			window.setWidth(w);
+			window.setHeight(h);
 		}
 		
 		private final double[] stageArgs() {
@@ -311,17 +392,21 @@ public class DraggableWindow<T extends Pane> extends Window<StackPane> {
 		}
 		
 		private final double scaledRoundX(double value) {
-			return scaledRound(value, stage.getRenderScaleX());
+			return scaledRound(value, window.getRenderScaleX());
 		}
 		
 		private final double scaledRoundY(double value) {
-			return scaledRound(value, stage.getRenderScaleY());
+			return scaledRound(value, window.getRenderScaleY());
 		}
 		
 		private final void mousePressed(MouseEvent event) {
-			if(!isDragTarget(event.getTarget())) return;
+			if(!isDragTarget(event.getTarget())) {
+				return;
+			}
+			
 			Bounds bounds = stageBounds();
 			Point2D mouse = mousePoint(event);
+			
 			if(isInDragBorder(bounds, mouse)) {
 				Point2D mouseScreen = mousePointScreen(event);
 				mx = scaledRoundX(mouseScreen.getX());
@@ -337,23 +422,29 @@ public class DraggableWindow<T extends Pane> extends Window<StackPane> {
 		
 		private final void mouseMoved(MouseEvent event) {
 			boolean inDragBorder = false;
+			
 			if(isDragTarget(event.getTarget())) {
 				Bounds bounds = stageBounds();
 				Point2D mouse = mousePoint(event);
+				
 				if(isInDragBorder(bounds, mouse)) {
 					Direction direction = mouseDragDirection(bounds, mouse);
-					stage.getScene().setCursor(direction.cursor());
+					window.getScene().setCursor(direction.cursor());
 					inDragBorder = true;
 					isCursorChanged = true;
 				}
 			}
+			
 			if(!inDragBorder && isCursorChanged) {
-				stage.getScene().setCursor(Cursor.DEFAULT);
+				window.getScene().setCursor(Cursor.DEFAULT);
 			}
 		}
 		
 		private final void mouseDragged(MouseEvent event) {
-			if(dragDirection == null) return;
+			if(dragDirection == null) {
+				return;
+			}
+			
 			saveStageArgs(transform(dragDirection.matrix(), stageArgs(), mouseDelta(event)));
 			event.consume();
 		}
@@ -375,8 +466,10 @@ public class DraggableWindow<T extends Pane> extends Window<StackPane> {
 			private final double[] matrix;
 			
 			private Direction(Cursor cursor, double... matrix) {
-				if(Objects.requireNonNull(matrix).length != 4)
+				if(Objects.requireNonNull(matrix).length != 4) {
 					throw new IllegalArgumentException();
+				}
+				
 				this.cursor = Objects.requireNonNull(cursor);
 				this.matrix = matrix;
 			}
