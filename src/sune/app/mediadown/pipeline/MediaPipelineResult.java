44919cb2ce856577269ff8b0d1ad1a13419d1a10
Sune <sune@suneweb.net>
2023-07-23 00:39:42 +0200

    Removed PipelineTaskRegistry.

--- src/sune/app/mediadown/pipeline/MediaPipelineResult.java
+++ src/sune/app/mediadown/pipeline/MediaPipelineResult.java
@@ -2,10 +2,6 @@ package sune.app.mediadown.pipeline;
 
 import java.util.Objects;
 
-import sune.app.mediadown.pipeline.PipelineTaskRegistry.PipelineTaskInputData;
-import sune.app.mediadown.util.Utils;
-import sune.app.mediadown.util.Utils.Ignore;
-
 public final class MediaPipelineResult implements PipelineResult<DownloadPipelineResult> {
 	
 	/** @since 00.02.08 */
@@ -22,23 +18,6 @@ public final class MediaPipelineResult implements PipelineResult<DownloadPipelin
 	
 	@Override
 	public final PipelineTask<DownloadPipelineResult> process(Pipeline pipeline) throws Exception {
-		if(media.media().metadata().isProtected()) {
-			PipelineTaskInputData data = new PipelineTaskInputData(Utils.toMap(
-				"media", media.media(), "destination", media.destination(), "configuration", media.configuration()
-			));
-			PipelineTask<DownloadPipelineResult> task
-				= Ignore.defaultValue(() -> PipelineTaskRegistry.instance("ProtectedMediaPipelineTask", data), (PipelineTask<DownloadPipelineResult>) null);
-			
-			if(task == null) {
-				throw new IllegalStateException("No pipeline task found for protected media.");
-			}
-			
-			// Notify, regardless of the task
-			media.submit();
-			
-			return task;
-		}
-		
 		return DownloadPipelineTask.of(media);
 	}
 	
