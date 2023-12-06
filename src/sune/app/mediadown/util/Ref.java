8521d06dd9bcffd7a2411bfda73441f641ad1dec
Sune <sune@suneweb.net>
2023-12-06 08:26:54 +0100

    Added Ref class for holding values, either mutable or immutable.

---
+++ src/sune/app/mediadown/util/Ref.java
@@ -0,0 +1,39 @@
+package sune.app.mediadown.util;
+
+/** @since 00.02.09 */
+public interface Ref<T> {
+	
+	T get();
+	
+	static class Immutable<T> implements Ref<T> {
+		
+		private final T value;
+		
+		public Immutable(T value) {
+			this.value = value;
+		}
+		
+		@Override
+		public T get() {
+			return value;
+		}
+	}
+	
+	static class Mutable<T> implements Ref<T> {
+		
+		private T value;
+		
+		public Mutable(T value) {
+			this.value = value;
+		}
+		
+		public void set(T value) {
+			this.value = value;
+		}
+		
+		@Override
+		public T get() {
+			return value;
+		}
+	}
+}
