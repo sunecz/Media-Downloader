970d854623624414bb92e80cc2b51a82b3fb5c77
Sune <sune@suneweb.net>
2024-02-01 03:43:44 +0100

    Updated the VideoUtils class.

--- src/sune/app/mediadown/util/VideoUtils.java
+++ src/sune/app/mediadown/util/VideoUtils.java
@@ -1,42 +1,53 @@
 package sune.app.mediadown.util;
 
 import java.nio.file.Path;
+import java.util.List;
 
 import sune.api.process.ReadOnlyProcess;
 import sune.app.mediadown.ffmpeg.FFprobe;
+import sune.app.mediadown.media.MediaConstants;
 
 public final class VideoUtils {
 	
-	private static final String COMMAND_DURATION;
+	private static final String COMMAND_DURATION = ""
+		+ " -v error"
+		+ " -show_entries format=duration"
+		+ " -of default=noprint_wrappers=1:nokey=1"
+		+ " \"%{path}s\"";
 	
-	static {
-		COMMAND_DURATION = "-v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \"%s\"";
+	// Forbid anyone to create an instance of this class
+	private VideoUtils() {
 	}
 	
 	public static final double duration(Path file) {
 		try(ReadOnlyProcess process = FFprobe.createSynchronousProcess()) {
-			String command = String.format(COMMAND_DURATION, file.toAbsolutePath().toString());
+			String command = Utils.format(
+				COMMAND_DURATION,
+				"path", file.toAbsolutePath().toString()
+			);
+			
 			return Double.parseDouble(process.execute(command));
 		} catch(Exception ex) {
 			// Ignore
 		}
-		// Failed to get the duration of the file, return a special value
-		return Double.NEGATIVE_INFINITY;
+		
+		return MediaConstants.UNKNOWN_DURATION;
+	}
+	
+	/** @since 00.02.09 */
+	public static final double tryGetDuration(List<Path> files) {
+		for(Path file : files) {
+			double duration = duration(file);
+			
+			if(duration >= 0.0) {
+				return duration;
+			}
+		}
+		
+		return MediaConstants.UNKNOWN_DURATION;
 	}
 	
 	public static final double tryGetDuration(Path... files) {
-		for(Path file : files) {
-			double duration = duration(file);
-			// Check whether we succeeded to get the duration
-			if(!Double.isInfinite(duration))
-				return duration;
-				
-		}
-		// Failed to get the duration of the file, return a special value
-		return Double.NEGATIVE_INFINITY;
-	}
-	
-	// Forbid anyone to create an instance of this class
-	private VideoUtils() {
+		return tryGetDuration(List.of(files));
 	}
 }
