3decd0e288133c5b9564495a2deb162b3fdbc866
Sune <sune@suneweb.net>
2025-03-26 06:43:11 +0100

    Increase the version to 00.02.09-dev.23.

--- src/sune/app/mediadown/MediaDownloader.java
+++ src/sune/app/mediadown/MediaDownloader.java
@@ -154,8 +154,8 @@ import sune.util.ssdf2.SSDObject;
 public final class MediaDownloader {
 	
 	public static final String  TITLE   = "Media Downloader";
-	public static final Version VERSION = Version.of("00.02.09-dev.22");
-	public static final String  DATE    = "2024-04-08";
+	public static final Version VERSION = Version.of("00.02.09-dev.23");
+	public static final String  DATE    = "2025-03-26";
 	public static final String  AUTHOR  = "Sune";
 	public static final Image   ICON    = icon("app.png");
 	
